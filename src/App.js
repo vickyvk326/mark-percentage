d2595d1c814b0a56dab28b8ec28dad86158e1b4a
vickyvk326 <vigneshravi326@gmail.com>
2022-10-22 11:22:12 +0530

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+
+  const [average, setAverage] = useState("Marks of Subject1:88, Subject2:81, Subject3:93, Subject4:99, Subject5:78");
+  const [subject1, setSubject1] = useState(88);
+  const [subject2, setSubject2] = useState(81);
+  const [subject3, setSubject3] = useState(93);
+  const [subject4, setSubject4] = useState(99);
+  const [subject5, setSubject5] = useState(78);
+  const calculate = ()=>{
+    setAverage((subject1+subject2+subject3+subject4+subject5)/5 + "%")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {average}
+      <button onClick={calculate}>Calculate</button>
       </header>
     </div>
   );
